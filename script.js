0bd19e9dcfaca119caec6f4c962ddefde54cb925
Luks <150530240+Luks14@users.noreply.github.com>
2024-08-16 12:00:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function mostrar_b(){
+    document.getElementById('img_b').style.display = 'block';
+}
+function tirar_b(){
+    document.getElementById('img_b').style.display = 'none';
+}
+
+
+
+
+
+function mostrar_s(){
+    document.getElementById('img_s').style.display = 'block';
+}
+function tirar_s(){
+    document.getElementById('img_s').style.display = 'none';
+}
+
+
+
+
+
+function mostrar_u(){
+    document.getElementById('img_u').style.display = 'block';
+}
+function tirar_u(){
+    document.getElementById('img_u').style.display = 'none';
+}
+
+
+
+
+
+function mostrar_a(){
+    document.getElementById('img_a').style.display = 'block';
+}
+function tirar_a(){
+    document.getElementById('img_a').style.display = 'none';
+}
